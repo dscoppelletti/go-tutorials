c120ac1bc55f103ac32d84e8510d7235fd29aec2
Dario Scoppelletti <dario@scoppelletti.it>
2020-12-16 17:31:15 +0100

    Return a random greeting

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -3,7 +3,9 @@ package greetings
 
 import (
     "errors"
-    "fmt"
+	"fmt"
+	"math/rand"
+    "time"
 )
 
 // Hello returns a greeting for the named person.
@@ -23,8 +25,39 @@ func Hello(name string) (string, error) {
 	// In Go, the := operator is a shortcut for declaring and initializing a
 	// variable in one line (Go uses the value on the right to determine the
 	// variable's type).
+	// Create a message using a random format.
 	// Sprintf substitutes the name parameter's value for the %v format verb.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
 	// The second returned value "nil" means no error.
     return message, nil
+}
+
+// init sets initial values for variables used in the function.
+// Go executes init functions automatically at program startup, after global
+// variables have been initialized.
+func init() {
+	// Seed the rand package with the current time.
+    rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned message
+// is selected at random.
+// The name of this function starts with a lowercase letter, making it
+// accessible only to code in its own package.
+func randomFormat() string {
+	// A slice of message formats.
+	// A slice is like an array, except that it's dynamically sized as you add
+	// and remove items.
+	// When declaring a slice, you omit its size in the brackets, so that the
+	// array underlying a slice can be dynamically sized.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+	// Return a randomly selected message format by specifying a random index
+	// for the slice of formats.
+	// the math/rand package to generates a random number
+    return formats[rand.Intn(len(formats))]
 }
--- create-module/hello/hello.go
+++ create-module/hello/hello.go
@@ -20,7 +20,7 @@ func main() {
     log.SetFlags(0)
 
     // Request a greeting message.
-    message, err := greetings.Hello("")
+    message, err := greetings.Hello("Gladys")
     // If an error was returned, print it to the console and exit the program.
     if err != nil {
         // Print the error and stop the program.
