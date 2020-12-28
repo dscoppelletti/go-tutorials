1aa4c5a0343a09a22b87a8ead959db3a2f8e1c87
Dario Scoppelletti <dario@scoppelletti.it>
2020-12-28 11:19:17 +0100

    Writing Web Applications
     - Handling non-existent pages
    
     - Saving Pages
    
     - Error handling
    
     - Template caching
    
     - Validation

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 //  A wiki consists of a series of interconnected pages, each of which has a
@@ -49,35 +51,70 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+// The function Must is a convenience wrapper that panics when passed a non-nil
+// error value, and otherwise returns the *Template unaltered.
+// The ParseFiles function takes any number of string arguments that identify
+// our template files, and parses those files into templates that are named
+// after the base file name.
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	// The html/template package is part of the Go standard library: we can use
-	// html/template to keep the HTML in a separate file, allowing us to change
-	// the layout of our edit page without modifying the underlying Go code.
-	// The function ParseFiles reads the contents of edit.html and return a
-	// *Template.
-	t, _ := template.ParseFiles(tmpl + ".html")
 	// The method Execute executes the template, writing the generated HTML to
 	// the ResponseWriter
-    t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+// Validation expression for the title.
+// The function MustCompile parses and compile the regular expression, and
+// return a Regexp. MustCompile is distinct from Compile in that it will panic
+// if the expression compilation fails, while Compile returns an error as a
+// second parameter.
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+// The function getTitle uses the validPath expression to validate path and
+// extract the page title.
+// If the title is valid, it will be returned along with a nil error value. If
+// the title is invalid, the function will write a "404 Not Found" error to the
+// HTTP connection, and return an error to the handler.
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("invalid Page Title")
+    }
+    return m[2], nil // The title is the second subexpression.
 }
 
 // The function viewHandler allow users to view a wiki page; it will handle URLs
 // prefixed with "/view/".
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	// Extract the page title from r.URL.Path, the path component of the request
-	// URL. The Path is re-sliced with [len("/view/"):] to drop the leading
-	// "/view/" component of the request path; this is because the path will
-	// invariably begin with "/view/", which is not part of the page's title.
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
 	// Load the page data.
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		// If the requested Page doesn't exist, it redirects the client to the
+		// edit Page so the content may be created.
+		// The Redirect function adds an HTTP status code 302 and a Location
+		// header to the HTTP response.
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
 // The function editHandler loads the page (or, if it doesn't exist, create an
 // empty Page struct), and displays an HTML form.
 func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
@@ -85,8 +122,32 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+// The function saveHandler handles the submission of forms located on the edit
+// pages.
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	// The page title (provided in the URL) and the form's only field, Body, are
+	// stored in a new Page.
+	title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+	body := r.FormValue("body")
+	// The value returned by FormValue is of type string, so we must convert
+	// that value to []byte before it will fit into the Page struct.
+	p := &Page{Title: title, Body: []byte(body)}
+	// The save() method writes the data to a file
+	err = p.save()
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+	// The client is redirected to the /view/ page.
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
